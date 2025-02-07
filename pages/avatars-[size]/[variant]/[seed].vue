9a8df0a496a324cdce9051c5d6f49a5c4ca4620b
mq1 <74494945+chan-mai@users.noreply.github.com>
2025-02-07 09:43:20 +0000

    feat: 動的生成

---
+++ pages/avatars-[size]/[variant]/[seed].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import Avatar from "vue-boring-avatars";
+
+    const route = useRoute();
+    const size: Ref<number> =  ref(parseInt(route.params.size as string ?? "200"));
+    const variant: Ref<"bauhaus" | "beam" | "marble" | "pixel" | "ring" | "sunset"> = ref(route.params.variant ? route.params.variant as "bauhaus" | "beam" | "marble" | "pixel" | "ring" | "sunset" : "beam");
+    const seed: Ref<string> =  ref(route.params.seed as string ?? "");
+</script>
+<template>
+    <Avatar
+    :size="size"
+    :variant="variant"
+    :name="seed"
+    :square="true"
+    :title="true"
+  />
+</template>
